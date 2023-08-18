ba36c342cf2425dbb0a6e72ad057f274cd75edb4
GOPINATH SAHU <135028348+gopinathsahu@users.noreply.github.com>
2023-08-18 19:26:14 +0530

    first commit

---
+++ sacript.js
@@ -0,0 +1,17 @@
+setInterval(()=>{
+date=new Date();
+
+htime=date.getHours();
+mtime=date.getMinutes();
+stime=date.getSeconds();
+digital.innerHTML=Date()
+
+hrotation=30*htime+mtime/2
+mrotation=6*mtime
+srotation=6*stime
+hour.style.transform=`rotate(${hrotation}deg)`;
+minute.style.transform=`rotate(${mrotation}deg)`;
+second.style.transform=`rotate(${srotation}deg)`;
+
+
+},1)
